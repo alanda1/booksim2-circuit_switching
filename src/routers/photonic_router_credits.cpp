63abac59dabddbce48455d5bb254c04462ebdbbf
Arjun Deopujari <arjund7@stanford.edu>
2022-03-12 02:39:57 -0500

    photonic_router_credits.cpp: Eliminate vca, sa, st pipeline stages for datapath

--- src/routers/photonic_router_credits.cpp
+++ src/routers/photonic_router_credits.cpp
@@ -271,30 +271,22 @@ void PhotonicRouterCredits::_InternalStep() {
 
   if (!_route_vcs.empty())
     _RouteEvaluate();
+  if (!_route_vcs.empty()) {
+    _RouteUpdate();
+    activity = activity || !_route_vcs.empty();
+  }
   if (_vc_allocator) {
     _vc_allocator->Clear();
     if (!_vc_alloc_vcs.empty())
       _VCAllocEvaluate();
   }
-  if (_hold_switch_for_packet) {
-    if (!_sw_hold_vcs.empty())
-      _SWHoldEvaluate();
-  }
-  _sw_allocator->Clear();
-  if (_spec_sw_allocator)
-    _spec_sw_allocator->Clear();
-  if (!_sw_alloc_vcs.empty())
-    _SWAllocEvaluate();
-  if (!_crossbar_flits.empty())
-    _SwitchEvaluate();
-
-  if (!_route_vcs.empty()) {
-    _RouteUpdate();
-    activity = activity || !_route_vcs.empty();
-  }
   if (!_vc_alloc_vcs.empty()) {
     _VCAllocUpdate();
     activity = activity || !_vc_alloc_vcs.empty();
+  }
+  if (_hold_switch_for_packet) {
+    if (!_sw_hold_vcs.empty())
+      _SWHoldEvaluate();
   }
   if (_hold_switch_for_packet) {
     if (!_sw_hold_vcs.empty()) {
@@ -302,10 +294,17 @@ void PhotonicRouterCredits::_InternalStep() {
       activity = activity || !_sw_hold_vcs.empty();
     }
   }
+  _sw_allocator->Clear();
+  if (_spec_sw_allocator)
+    _spec_sw_allocator->Clear();
+  if (!_sw_alloc_vcs.empty())
+    _SWAllocEvaluate();
   if (!_sw_alloc_vcs.empty()) {
     _SWAllocUpdate();
     activity = activity || !_sw_alloc_vcs.empty();
   }
+  if (!_crossbar_flits.empty())
+    _SwitchEvaluate();
   if (!_crossbar_flits.empty()) {
     _SwitchUpdate();
     activity = activity || !_crossbar_flits.empty();
@@ -705,7 +704,7 @@ void PhotonicRouterCredits::_RouteUpdate() {
     pair<int, pair<int, int> > const &item = _route_vcs.front();
 
     int const time = item.first;
-    if ((time < 0) || (GetSimTime() < time)) {
+    if ((time < 0) /*|| (GetSimTime() < time)*/) {
       break;
     }
     assert(GetSimTime() == time);
@@ -1097,7 +1096,7 @@ void PhotonicRouterCredits::_VCAllocUpdate() {
     pair<int, pair<pair<int, int>, int> > const &item = _vc_alloc_vcs.front();
 
     int const time = item.first;
-    if ((time < 0) || (GetSimTime() < time)) {
+    if ((time < 0) /*|| (GetSimTime() < time)*/) {
       break;
     }
     assert(GetSimTime() == time);
@@ -2066,7 +2065,7 @@ void PhotonicRouterCredits::_SWAllocUpdate() {
     pair<int, pair<pair<int, int>, int> > const &item = _sw_alloc_vcs.front();
 
     int const time = item.first;
-    if ((time < 0) || (GetSimTime() < time)) {
+    if ((time < 0) /*|| (GetSimTime() < time)*/) {
       break;
     }
     assert(GetSimTime() == time);
@@ -2385,7 +2384,7 @@ void PhotonicRouterCredits::_SwitchUpdate() {
     pair<int, pair<Flit *, pair<int, int> > > const &item = _crossbar_flits.front();
 
     int const time = item.first;
-    if ((time < 0) || (GetSimTime() < time)) {
+    if ((time < 0) /*|| (GetSimTime() < time)*/) {
       break;
     }
     assert(GetSimTime() == time);
